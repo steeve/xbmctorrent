65fb294c17130985e6549702a1df728126de8cb0
Steeve Morin <steeve.morin@gmail.com>
2013-10-01 01:48:54 +0200

    Make sure we use our own vendored packages

--- addon.py
+++ addon.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
 from xbmctorrent import plugin
 
 
