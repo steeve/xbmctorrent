b1ecf89c4b856cf8d248116c0fbbffa2d4a05617
Steeve Morin <steeve.morin@gmail.com>
2013-12-03 00:26:25 +0100

    Supplemental error logging (on Android this is needed). Closes #36

--- addon.py
+++ addon.py
@@ -2,6 +2,11 @@ import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
 from xbmctorrent import plugin
 
-
 if __name__ == '__main__':
-    plugin.run()
+    try:
+        plugin.run()
+    except Exception, e:
+        import xbmc
+        import traceback
+        map(xbmc.log, line in traceback.format_exc().split("\n"))
+        raise
