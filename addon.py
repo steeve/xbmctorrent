e5150da777311bb583a574120c46a26df054ec8c
Steeve Morin <steeve.morin@gmail.com>
2014-02-04 03:31:02 +0100

    Ensure monkey patches loading

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
-from xbmctorrent import plugin
+from xbmctorrent import monkey_patches, plugin
 
 if __name__ == '__main__':
     try:
